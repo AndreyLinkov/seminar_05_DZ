820fb76601e62736778f326074c93bc7b881d6c3
Andrey Linkov <linkov.developer@inbox.ru>
2023-12-03 19:43:04 +0300

    add task_01

---
+++ task_01/Program.cs
@@ -0,0 +1,52 @@
+﻿// Написать программу, которая бесконечно запрашивает целые числа с консоли.
+// Программа завершается при вводе символы 'q' или при вводе числа, сумма цифр 
+// которого четная
+
+Console.Clear();
+void GenerateArray(int[] arr, string num)
+{
+    for (int i = 0; i < num.Length; i++)
+    {
+        arr[i] = (int)char.GetNumericValue(num[i]);
+    }
+}
+
+bool IsEvenSum(int[] arr)
+{
+    int sum = 0;
+    foreach (int digit in arr)
+    {
+        sum += digit;
+    }
+    return sum % 2 == 0;
+}
+
+Console.Write("Continue entering an integer until the program stops. You can stop the program by pressing the 'q' button or until the sum of the digits of your number is even: ");
+string input = "";
+
+do 
+{
+    input = Console.ReadLine();
+    if (input.ToLower()=="q") 
+    {
+        Console.WriteLine("[STOP]");
+        break;
+    }
+
+    int number;
+    if (int.TryParse(input, out number))
+    {
+        string numStr = number.ToString();
+        int length = numStr.Length;
+        int[] array = new int[length];
+
+        GenerateArray(array, numStr);
+        bool isEven = IsEvenSum(array);
+
+        if (isEven)
+        {
+            Console.WriteLine("[STOP]");
+            break;
+        }
+    } 
+} while (true);
